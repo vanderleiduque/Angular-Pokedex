a0a340cbafd9d43eea7cc1e6115a136f02a29dc0
Vanderlei Duque Ferreira <vanderlei.duque@w12.com.br>
2023-04-01 03:24:36 -0300

    Integração com API

---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+  pokemons = [];
+
+  constructor( private httpClient: HttpClient) {
+    this.carregarPokemons()
+  }
+  
+  async carregarPokemons() {
+    debugger
+    const requisicao = await this.httpClient
+      .get<any>('https://pokeapi.co/api/v2/pokemon?limit=151')
+      .toPromise();
+
+    this.pokemons = requisicao.results;
+    
+  }
+}
